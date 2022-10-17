27db27899f2dd6b5aa2623636553eb2444294214
Jangs <wkdwnsdlr37@gmail.com>
2022-10-17 20:28:39 +0900

    업데이트

--- 익스프레스서버/statesairline/controller/bookController.js
+++ 익스프레스서버/statesairline/controller/bookController.js
@@ -14,6 +14,8 @@ module.exports = {
   // 요청 된 phone과 동일한 phone 예약 데이터를 조회합니다.
   findByPhone: (req, res) => {
     const {phone} = req.params;
+    const data = booking.filter(book => book.phone === phone);
+    return res.status(200).json(data);
 
   },
   // [GET] /book/:phone/:flight_uuid 요청을 수행합니다.
@@ -21,6 +23,8 @@ module.exports = {
   findByPhoneAndFlightId: (req,res) => {
     const {phone, flight_uuid} = req.params;
     // TODO:
+    const data = booking.filter(book => book.phone === phone && book.flight_uuid === flight_uuid);
+      return res.status(200).json(data);
 
     
   },
@@ -30,6 +34,17 @@ module.exports = {
     // POST /book에서 사용할 booking_uuid입니다.
     const booking_uuid = uuid();
     // TODO:
+    const {flight_uuid, name, phone} = req.body;
+    // uuid, name, phone -> booking 배열 저장
+    const data = {
+      booking_uuid: booking_uuid,
+      flight_uuid: flight_uuid,
+      name: name,
+      phone: phone
+    };
+    booking.push(data);
+    // console.log(req)
+    return res.json(booking);
   },
 
   // Optional
@@ -37,6 +52,8 @@ module.exports = {
   // 요청 된 id, phone 값과 동일한 예약 데이터를 삭제합니다.
   deleteByBookingId: (req, res) => {
     const {booking_uuid} = req.params;
+    booking = booking.filter(book => book.booking_uuid !== booking_uuid);
+    return res.status(200).json({booking_uuid});
     // TODO:
   }
 };
--- 익스프레스서버/statesairline/controller/flightController.js
+++ 익스프레스서버/statesairline/controller/flightController.js
@@ -9,8 +9,6 @@ module.exports = {
     const { departure_times, arrival_times, destination, departure } = req.query;
     // TODO:
 
-    
-
       
     if (departure_times && arrival_times) {
       let resultList = flights.filter((data) => {
@@ -29,7 +27,7 @@ module.exports = {
       return res.json(resultList);
     }
 
-      return res.json(flights)
+    return res.json(flights)
 
   },
   // [GET] /flight/:ㅕㅕid
@@ -37,14 +35,23 @@ module.exports = {
   findById: (req, res) => {
     const { uuid } = req.params;
     // TODO:
+    const result = flights.filter((data)=>{
+      if(data.uuid === uuid){
+        return true;
+      }
+    });
 
+    return res.json(result);
+
+    // test : af6fa55c-da65-47dd-af23-578fdba44bed
     // return res.json(flights.filter((data)=>{
     //   if(data.uuid === uuid){
     //     return true;
     //   }
     // }));
 
-    return res.json(flights)
+
+    // return res.json(flights)
   },
 
   
@@ -59,11 +66,16 @@ module.exports = {
   // [PUT] /flight/:uuid 요청을 수행합니다.
   // 요청 된 uuid 값과 동일한 uuid 값을 가진 항공편 데이터를 요쳥 된 Body 데이터로 수정합니다.
   update: (req, res) => {
-    const { uuid } = req.params;
-    const bodyData = req.body;
+    
+    
      // TODO:
+    //  const { uuid } = req.params;
+    //  const bodyData = req.body;
+    //  const beUpdatedIdx = flights.findIndex(flight => flight.uuid === uuid);
+    //  const updatedFlight = { ...flights[beUpdatedIdx], ...bodyData }; //최종적으로 업데이트된 flight
+    //  flights.splice(beUpdatedIdx, 1, updatedFlight);
 
-     return res.json(flights);
+    //  return res.status(200).json(updatedFlight);
   }
 };
 
