ecd239dc4b23b9d71a1e1504ac000d830b4614a8
Jangs <wkdwnsdlr37@gmail.com>
2022-10-31 18:34:26 +0900

    Update

---
+++ react컴포넌튼(storybook)/src/components/BareMinimumRequirements/Modal.js
@@ -0,0 +1,79 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+export const ModalContainer = styled.div`
+  // TODO : Modal을 구현하는데 전체적으로 필요한 CSS를 구현합니다.
+  outline: 1px solid red;
+  text-align: center;
+  width: 100%;
+  height: 100%;
+  position: relative;
+`;
+
+export const ModalBackdrop = styled.div`
+  // TODO : Modal이 떴을 때의 배경을 깔아주는 CSS를 구현합니다.
+  outline: 1px solid blue;
+  background-color: rgba(0, 0, 0, 0.195);
+  position: absolute;
+  top:0;
+  left:0;
+  bottom:0;
+  right:0;
+  // z-index: 1;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ModalBtn = styled.button`
+  background-color: var(--coz-purple-600);
+  text-decoration: none;
+  border: none;
+  padding: 20px;
+  color: white;
+  border-radius: 30px;
+  cursor: pointer;
+  margin-top: 110px;
+`;
+
+export const ModalView = styled.div.attrs((props) => ({
+  // attrs 메소드를 이용해서 아래와 같이 div 엘리먼트에 속성을 추가할 수 있습니다.
+  role: 'dialog',
+}))`
+  // TODO : Modal창 CSS를 구현합니다.
+  background-color: white;
+  width: 300px;
+  height: 200px;
+
+`;
+
+export const Modal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModalHandler = () => {
+    // TODO : isOpen의 상태를 변경하는 메소드를 구현합니다.
+    setIsOpen(!isOpen)
+
+  };
+
+  return (
+    <>
+      <ModalContainer>
+        
+        <ModalBtn onClick={openModalHandler}>
+        {/* // TODO : 클릭하면 Modal이 열린 상태(isOpen)를 boolean 타입으로 변경하는 메소드가 실행되어야 합니다. */}
+          {isOpen ? 'Opened!' : 'Open Modal'}
+          {/* TODO : 조건부 렌더링을 활용해서 Modal이 열린 상태(isOpen이 true인 상태)일 때는 ModalBtn의 내부 텍스트가 'Opened!' 로 Modal이 닫힌 상태(isOpen이 false인 상태)일 때는 ModalBtn 의 내부 텍스트가 'Open Modal'이 되도록 구현해야 합니다. */}
+          {isOpen ? <ModalBackdrop>
+          <ModalView>
+              <button>x</button>
+              <p>모달창</p>
+          </ModalView>
+        </ModalBackdrop> : null}
+        </ModalBtn>
+        {/* TODO : 조건부 렌더링을 활용해서 Modal이 열린 상태(isOpen이 true인 상태)일 때만 모달창과 배경이 뜰 수 있게 구현해야 합니다. */}
+       
+      </ModalContainer>
+    </>
+  );
+};
---
+++ react컴포넌튼(storybook)/src/components/BareMinimumRequirements/Toggle.js
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const ToggleContainer = styled.div`
+  position: relative;
+  margin-top: 8rem;
+  left: 47%;
+  cursor: pointer;
+
+  > .toggle-container {
+    position: relative;
+    left:0;
+    width: 50px;
+    height: 24px;
+    border-radius: 30px;
+    background-color: #8b8b8b;
+    transition: 2s;
+    // TODO : .toggle--checked 클래스가 활성화 되었을 경우의 CSS를 구현합니다.
+    &.toggle--checked{
+      background-color: red;
+
+      // &::before{
+      //   content: "";
+      //   display:block;
+      //   background-color: black;
+      //   position: absolute;
+      //   left:0;
+      //   width:0;
+      // }
+
+      // $:active{
+      //   width:100%;
+      // }
+  }
+  
+  }
+
+  > .toggle-circle {
+    position: absolute;
+    top: 1px;
+    left: 1px;
+    width: 22px;
+    height: 22px;
+    border-radius: 50%;
+    background-color: #ffffff;
+    transition: 1s;
+    // TODO : .toggle--checked 클래스가 활성화 되었을 경우의 CSS를 구현합니다.
+    &.toggle--checked{
+      left:27px;
+      background-color:blue;
+     
+    }
+  }
+`;
+
+const Desc = styled.div`
+  // TODO : 설명 부분의 CSS를 구현합니다.
+  text-align: center;
+  // outline: 1px solid red;
+`;
+
+export const Toggle = () => {
+  const [isOn, setisOn] = useState(false);
+
+  const toggleHandler = () => {
+    // TODO : isOn의 상태를 변경하는 메소드를 구현합니다.
+    setisOn(!isOn)
+  };
+
+  return (
+    <>
+      <ToggleContainer onClick={toggleHandler}
+        // TODO : 클릭하면 토글이 켜진 상태(isOn)를 boolean 타입으로 변경하는 메소드가 실행되어야 합니다.
+      >
+        {/* TODO : 아래에 div 엘리먼트 2개가 있습니다. 각각의 클래스를 'toggle-container', 'toggle-circle' 로 지정하세요. */}
+        {/* TIP : Toggle Switch가 ON인 상태일 경우에만 toggle--checked 클래스를 div 엘리먼트 2개에 모두 추가합니다. 조건부 스타일링을 활용하세요. */}
+        <div className={`toggle-container ${isOn ? "toggle--checked" : null}`}/>
+        <div className={`toggle-circle ${isOn ? "toggle--checked" : null}`}/>
+      </ToggleContainer>
+      {/* TODO : Desc 컴포넌트를 활용해야 합니다. */}
+      {/* TIP:  Toggle Switch가 ON인 상태일 경우에 Desc 컴포넌트 내부의 텍스트를 'Toggle Switch ON'으로, 그렇지 않은 경우 'Toggle Switch OFF'가 됩니다. 조건부 렌더링을 활용하세요. */}
+      <Desc>{isOn ? 'Toggle Switch ON' : 'Toggle Switch OFF'}</Desc>
+    </>
+  );
+};
