9e0223b7ffd5938d8ff0f55d174c24dda26e187e
Jangs <wkdwnsdlr37@gmail.com>
2022-11-07 14:10:25 +0900

    업데이트

--- react컴포넌튼(storybook)/src/components/AdvancedChallenges/Autocomplete.js
+++ react컴포넌튼(storybook)/src/components/AdvancedChallenges/Autocomplete.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
+
+// 추천검색을 띄어줄 데이터
 const deselectedOptions = [
   'rustic',
   'antique',
@@ -14,11 +16,12 @@ const deselectedOptions = [
   '골동품'
 ];
 
-/* TODO : 아래 CSS를 자유롭게 수정하세요. */
+//  스타일 컴포넌트에 넣어주기위한 css변수
 const boxShadow = '0 4px 6px rgb(32 33 36 / 28%)';
 const activeBorderRadius = '1rem 1rem 0 0';
 const inactiveBorderRadius = '1rem 1rem 1rem 1rem';
 
+// div 컴포넌트
 export const InputContainer = styled.div`
   margin-top: 8rem;
   background-color: #ffffff;
@@ -26,9 +29,10 @@ export const InputContainer = styled.div`
   flex-direction: row;
   padding: 1rem;
   border: 1px solid rgb(223, 225, 229);
-  border-radius: ${inactiveBorderRadius};
+  border-radius: ${(props) =>
+    props.hasText ? activeBorderRadius : inactiveBorderRadius};
   z-index: 3;
-  box-shadow: 0;
+  box-shadow: ${(props) => (props.hasText ? boxShadow : 0)};
 
   &:focus-within {
     box-shadow: ${boxShadow};
@@ -49,6 +53,7 @@ export const InputContainer = styled.div`
   }
 `;
 
+// ul컴포넌트
 export const DropDownContainer = styled.ul`
   background-color: #ffffff;
   display: block;
@@ -69,164 +74,121 @@ export const DropDownContainer = styled.ul`
 
   > li {
     padding: 0 1rem;
+
+    &:hover {
+      background-color: #eee;
+    }
+
+    &.selected {
+      background-color: #ebe5f9;
+    }
   }
 `;
 
+// Autocomplete => 화면에 보여질~
 export const Autocomplete = () => {
-  /**
-   * Autocomplete 컴포넌트는 아래 3가지 state가 존재합니다. 필요에 따라서 state를 더 만들 수도 있습니다.
-   * - hasText state는 input값의 유무를 확인할 수 있습니다.
-   * - inputValue state는 input값의 상태를 확인할 수 있습니다.
-   * - options state는 input값을 포함하는 autocomplete 추천 항목 리스트를 확인할 수 있습니다.
-   */
+
   const [hasText, setHasText] = useState(false);
   const [inputValue, setInputValue] = useState('');
   const [options, setOptions] = useState(deselectedOptions);
-  const [sel , setSel] = useState(-1);
+  const [selected, setSelected] = useState(-1);
 
-  // useEffect를 아래와 같이 활용할 수도 있습니다.
+  // inputValue의 값이 변화할때마다 useEffect를 실행한다(중속성)
+  // 만약 inputValue의 값이 빈문자열이라면 hasText의 값을 false로 @@@@@@@@@???
   useEffect(() => {
     if (inputValue === '') {
       setHasText(false);
     }
-
-    if(inputValue !== ""){
-      setOptions(deselectedOptions.filter((el)=>{
-        return el.includes(inputValue)
-      }))
-    }
   }, [inputValue]);
 
-  // TODO : input과 dropdown 상태 관리를 위한 handler가 있어야 합니다.
+
+  
   const handleInputChange = (event) => {
-    // let result = event.target.value
-    // setInputValue(result);
-    // setHasText(true);
+    const { value } = event.target;
     
-    
+    // 역슬래쉬를 쓸수 없게 금지
+    if (value.includes('\\')) return;
 
-    
-    
-    
-    
+    // input에 텍스트가 있는지 없는지 확인하는 코드
+    value ? setHasText(true) : setHasText(false);
 
-    /**
-     * handleInputChange 함수는
-     * - input값 변경 시 발생되는 change 이벤트 핸들러입니다.
-     * - input값과 상태를 연결시킬 수 있게 controlled component로 만들 수 있고
-     * - autocomplete 추천 항목이 dropdown으로 시시각각 변화되어 보여질 수 있도록 상태를 변경합니다.
-     *
-     * handleInputChange 함수를 완성하여 아래 3가지 기능을 구현합니다.
-     *
-     * onChange 이벤트 발생 시
-     * 1. input값 상태인 inputValue가 적절하게 변경되어야 합니다.
-     * 2. input값 유무 상태인 hasText가 적절하게 변경되어야 합니다.
-     * 3. autocomplete 추천 항목인 options의 상태가 적절하게 변경되어야 합니다.
-     * Tip : options의 상태에 따라 dropdown으로 보여지는 항목이 달라집니다.
-     */
-    let inputText = event.target.value;
-    setInputValue(inputText);
-    setHasText(true);
-    setOptions(deselectedOptions.filter((el)=> el.includes(inputText)))
+    // updateText
+    setInputValue(value);
 
+    // dropdown을 위한 기능
+    const filterRegex = new RegExp(value, 'i');
+    const resultOptions = deselectedOptions.filter((option) =>
+      option.match(filterRegex)
+    );
+    setOptions(resultOptions);
   };
 
   const handleDropDownClick = (clickedOption) => {
-    /**
-     * handleDropDownClick 함수는
-     * - autocomplete 추천 항목을 클릭할 때 발생되는 click 이벤트 핸들러입니다.
-     * - dropdown에 제시된 항목을 눌렀을 때, input값이 해당 항목의 값으로 변경되는 기능을 수행합니다.
-     *
-     * handleInputChange 함수를 완성하여 아래 기능을 구현합니다.
-     *
-     * onClick 이벤트 발생 시
-     * 1. input값 상태인 inputValue가 적절하게 변경되어야 합니다.
-     * 2. autocomplete 추천 항목인 options의 상태가 적절하게 변경되어야 합니다.
-     */
-    setInputValue(clickedOption)
-    setOptions(deselectedOptions.filter((el)=> el.includes(clickedOption)))
+    setInputValue(clickedOption);
+    const resultOptions = deselectedOptions.filter(
+      (option) => option === clickedOption
+    );
+    setOptions(resultOptions);
   };
 
   const handleDeleteButtonClick = () => {
-    
-    setInputValue("");
-
-    // setInputValue("");
-    /**
-     * handleDeleteButtonClick 함수는
-     * - input의 오른쪽에 있는 X버튼 클릭 시 발생되는 click 이벤트 핸들러입니다.
-     * - 함수 작성을 완료하여 input값을 한 번에 삭제하는 기능을 구현합니다.
-     *
-     * handleDeleteButtonClick 함수를 완성하여 아래 기능을 구현합니다.
-     *
-     * onClick 이벤트 발생 시
-     * 1. input값 상태인 inputValue가 빈 문자열이 되어야 합니다.
-     * 
-     */
-
-    
+    setInputValue('');
   };
 
- 
-
-
- 
-  
-
-  // Advanced Challenge: 상하 화살표 키 입력 시 dropdown 항목을 선택하고, Enter 키 입력 시 input값을 선택된 dropdown 항목의 값으로 변경하는 handleKeyUp 함수를 만들고,
-  // 적절한 컴포넌트에 onKeyUp 핸들러를 할당합니다. state가 추가로 필요한지 고민하고, 필요 시 state를 추가하여 제작하세요.
-
   const handleKeyUp = (event) => {
-    if(event.key === "ArrowDown" && sel < options.length -1){
-      setSel(sel + 1);
+    // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/getModifierState#example
+    // eslint-disable-next-line
+    if (event.getModifierState("Fn") || event.getModifierState("Hyper") || event.getModifierState("OS") || event.getModifierState("Super") || event.getModifierState("Win")) return; if (event.getModifierState("Control") + event.getModifierState("Alt") + event.getModifierState("Meta") > 1) return;
+    if (hasText) {
+      if (event.code === 'ArrowDown' && options.length - 1 > selected) {
+        setSelected(selected + 1);
+      }
+      if (event.code === 'ArrowUp' && selected >= 0) {
+        setSelected(selected - 1);
+      }
+      if (event.code === 'Enter' && selected >= 0) {
+        handleDropDownClick(options[selected]);
+        setSelected(-1);
+      }
     }
-    if(event.key === "ArrowUp" && sel > -1){
-      setSel(sel - 1);
-    }
-    if(event.key === "Enter"){
-      handleDropDownClick(options[sel]);
-      setSel(-1);
-    }
-  }
+  };
 
   return (
-    <div className='autocomplete-wrapper'>
-      <InputContainer>
-        {/* TODO : input 엘리먼트를 작성하고 input값(value)을 state와 연결합니다. handleInputChange 함수와 input값 변경 시 호출될 수 있게 연결합니다. */}
-        <input 
-          value={inputValue}
+    <div className='autocomplete-wrapper' onKeyUp={handleKeyUp}>
+      <InputContainer hasText={hasText}>
+        <input
+          type='text'
+          className='autocomplete-input'
           onChange={handleInputChange}
-          onKeyUp={handleKeyUp}
-          >
-
-          </input>
-        {/* TODO : 아래 div.delete-button 버튼을 누르면 input 값이 삭제되어 dropdown이 없어지는 handler 함수를 작성합니다. */}
-        <div className='delete-button' onClick={handleDeleteButtonClick}>&times;</div>
+          value={inputValue}
+        />
+        <div className='delete-button' onClick={handleDeleteButtonClick}>
+          &times;
+        </div>
       </InputContainer>
-      
-      {/* TODO : input 값이 없으면 dropdown이 보이지 않아야 합니다. 조건부 렌더링을 이용해서 구현하세요. */}
-     {hasText ?  <DropDown
-       options={options}
-       handleComboBox={handleDropDownClick} sel={sel}
-       /> : null}
+      {hasText ? (
+        <DropDown
+          options={options}
+          handleDropDownClick={handleDropDownClick}
+          selected={selected}
+        />
+      ) : null}
     </div>
   );
 };
 
-export const DropDown = ({ options, handleComboBox }) => {
+export const DropDown = ({ options, handleDropDownClick, selected }) => {
   return (
     <DropDownContainer>
-      {/* TODO : input 값에 맞는 autocomplete 선택 옵션이 보여지는 역할을 합니다. */}
-      {/* {options.map((ele)=>{
-        return(
-          <div onClick={()=>{
-            handleComboBox(ele)}}
-            > {ele}</div>
-        )
-      })} */}
-      {options.map((el) => <li onClick={()=>handleComboBox(el)}>{el}</li>)}
-      
-      
+      {options.map((option, idx) => (
+        <li
+          key={idx}
+          onClick={() => handleDropDownClick(option)}
+          className={selected === idx ? 'selected' : ''}
+        >
+          {option}
+        </li>
+      ))}
     </DropDownContainer>
   );
 };
